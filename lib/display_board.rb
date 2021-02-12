5dd3ca57545bf4afe9bef73532a3dd9bb5c0d612
colorful-state-34cc <seth623@gmail.com>
2021-02-12 04:19:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,8 +4,8 @@ def display_board(array = [' ',' ',' ',
                            ' ',' ',' ',
                            ' ',' ',' ',])
   puts " #{array[0]}  | #{array[1]}  | #{array[2]}  ",
-           "-----------",
-           " #{array[3]}  | #{array[4]}  | #{array[5]}  ",
-           "-----------",
-           " #{array[6]}  | #{array[7]}  | #{array[8]} "
+  puts         "-----------",
+  puts         " #{array[3]}  | #{array[4]}  | #{array[5]}  ",
+  puts         "-----------",
+  puts         " #{array[6]}  | #{array[7]}  | #{array[8]} "
 end
