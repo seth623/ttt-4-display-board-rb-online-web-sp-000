c4865867141945761745b7fba4ac177d659e8394
colorful-state-34cc <seth623@gmail.com>
2021-02-12 02:59:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(array = [' ','|',' ','|',' ',
+                           '-----------------',
+                           ' ','|',' ','|',' ',
+                           '-----------------',
+                           ' ','|',' ','|',' '])
+
+
+  array = ['#{array[0]}','|','#{array[1]}','|','#{array[2]}'
+           '-----------------',
+           '#{array[3]}','|','#{array[4]}','|','#{array[5]}',
+           '-----------------',
+           '#{array[6]}','|','#{array[7]}','|','#{array[8]}']
+  puts array
+end
